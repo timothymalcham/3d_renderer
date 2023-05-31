2148799b32c8beca48005df5c8288e41f63bb138
Timothy Malcham <timothymalcham@gmail.com>
2023-05-31 18:18:07 +0200

    create an SDL window

--- src/main.c
+++ src/main.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+
+bool is_running = false;
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+bool initialize_window(void) {
+    printf("Initializing window\n");
+
+    // if SDL_Init returns 0, then there was an error
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        fprintf(stderr, "Error initializing SDL: %s\n", SDL_GetError());
+        return false;
+    }
+
+    // create a SDL window
+    window = SDL_CreateWindow(
+        NULL,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        1920,
+        1080,
+        SDL_WINDOW_BORDERLESS
+    );
+
+    if (!window) {
+        fprintf(stderr, "Error creating SDL window: %s\n", SDL_GetError());
+        return false;
+    }
+
+    // Create a SDL renderer
+    renderer = SDL_CreateRenderer(window, -1, 0);
+
+    if (!renderer) {
+        fprintf(stderr, "Error creating SDL renderer: %s\n", SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
 
 int main(void) {
-    printf("Entry point\n");
+    is_running = initialize_window();
     return 0;
 }

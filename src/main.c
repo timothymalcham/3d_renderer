9b3090330c91f3da1ed18ecfe29d672e022baaed
Timothy Malcham <timothymalcham@gmail.com>
2023-06-02 16:36:57 +0200

    draw dotted grid on window

--- src/main.c
+++ src/main.c
@@ -94,6 +94,14 @@ void process_input(void) {
     }
 }
 
+void draw_dot_grid(void) {
+    for (int y = 0; y < window_height; y += 25) {
+        for (int x = 0; x < window_width; x += 25) {
+            color_buffer[(window_width * y) + x] = 0xFFFFFFFF;     
+        }
+    }
+}
+
 void update(void) {
     
 }
@@ -121,8 +129,10 @@ void render(void) {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
+    draw_dot_grid();
+
     render_color_buffer();
-    clear_color_buffer(0xFFFFFF00);
+    clear_color_buffer(0xFF000000);
 
     SDL_RenderPresent(renderer);
 }
